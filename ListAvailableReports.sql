5f057222c8654bdf83f984e8640b8b5557555d2d
Jason Lamm <94714566+jason-lamm@users.noreply.github.com>
2022-06-29 09:56:35 -0400

    Create ListAvailableReports.sql

---
+++ ListAvailableReports.sql
@@ -0,0 +1,12 @@
+Select   [Name],
+               
+               SubString([Path],1,Len([Path]) - (CharIndex('/',Reverse([Path]))-1)) As [Path],
+               Case
+                        When [Hidden] = 1
+                        Then  'Yes'
+                        Else    'No'
+               End As [Hidden]
+From    [Catalog]
+Where  [Type] = 2
+Order By SubString([Path],1,Len([Path]) - (CharIndex('/',Reverse([Path]))-1)),
+               [Name]
